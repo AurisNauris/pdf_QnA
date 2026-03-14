1fdbe2d7cb45c41bec5fa0c9a5251b3fce2f3380
Aurimas <aurimasnarkevicius@gmail.com>
2026-03-14 11:23:20 +0100

    Checking how a basic similarity would work

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pymupdf
 import sys
-from sentence_transformers import SentenceTransformer 
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import matplotlib.pyplot as plt
 
 def extract_text(file_path):
     doc = pymupdf.open(file_path)
@@ -23,15 +26,45 @@ def simple_chunking(pages):
      chunks[-1] = pages_str[-500:]
     return chunks
 
+def basic_similarity_check(list_of_sentences):
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    embeddings = model.encode(list_of_sentences)
+    sim_matrix = np.full(shape=(len(embeddings),len(embeddings)),fill_value=np.nan)
+    for i, em1 in enumerate(embeddings):
+        for j, em2 in enumerate(embeddings):
+            tmp_sim = cosine_similarity(em1.reshape(1,-1),em2.reshape(1,-1))
+            sim_matrix[i,j] = tmp_sim[0,0]
+    
+    tmp = sim_matrix.copy()
+    np.fill_diagonal(tmp, -1.0)
+    flat_sorted = np.argsort(tmp, axis=None)[::-1]
+    
+    for k in range(6):
+        i, j = np.unravel_index(flat_sorted[k], sim_matrix.shape)
+        if i>=j: # prevents duplicates
+            continue
+        print("-"*5,k,"-"*5)
+        print("Most similar sentences are:")
+        print(list_of_sentences[i])
+        print(list_of_sentences[j])
 
-file_path = sys.argv[1]
-extracted_pages = extract_text(file_path)
-chunks = simple_chunking(extracted_pages)
+    plt.figure()
+    plt.imshow(sim_matrix)
+    plt.show()
 
-print(chunks[0])
+#file_path = sys.argv[1]
+#extracted_pages = extract_text(file_path)
+#chunks = simple_chunking(extracted_pages)
+#embedding = model.encode(chunks[:2])
+#print("-"*10)
+#print(embedding)
+#print("embedding shape:", embedding.shape)
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
-embedding = model.encode(chunks[:2])
-print("-"*10)
-print(embedding)
-print("embedding shape:", embedding.shape)
+test_sentences = ["The cat sat on the mat",
+                 "The kitten was sitting on the rug",
+                 "Stock prices rose sharply today",
+                 "The mat sat on the cat",
+                 "The dog sat on the cat"
+                 ]
+
+basic_similarity_check(test_sentences)
